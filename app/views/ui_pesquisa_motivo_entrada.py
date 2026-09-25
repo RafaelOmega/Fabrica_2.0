d967f906feb7a6d035b6a5ee7d3d6bbe8ef9cfec
RafaelOmega <raffael.oliveira.823@gmail.com>
2026-09-25 11:12:54 -0300

    Implementação da Ficha Tecnica

--- app/views/ui_pesquisa_motivo_entrada.py
+++ app/views/ui_pesquisa_motivo_entrada.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'pesquisa_motivo_entradauTYfLM.ui'
+## Form generated from reading UI file 'pesquisa_motivo_entradaCvshZE.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.11.2
 ##
